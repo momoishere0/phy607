9dc4c4bfb090eaa75b630814c8decdf71c81ecb4
Mohammed Amal <mohammedamal@Mohammeds-MacBook-Air.local>
2025-09-18 13:11:03 -0400

    Add cow.py

--- cow.py
+++ cow.py
@@ -1,14 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-m = 1000
-dt = 0.01
+m = 10
+dt = 0.0001
 def get_int(): ## function gets the input variables
-    y0 = float(input("Intial hight:  "))
-    vx = float(input("Intial x Velocity:  "))
-    vy = float(input("Intial y Velocity:  "))
-    v0 = np.array([vx,vy])
-    df = float(input('Drag Ceofficent:  '))
-    return [y0,v0,df]
+    y0 = float(input("Intial displacement:  "))
+    v0 = float(input("Intial Velocity:  "))
+    k = float(input('spring constant:  '))
+    t = float(input('Total time:  '))
+    return [y0,v0,k,t]
 
 
 def force(y0,v0,df): ## function returns the force vector [FX,FY] at any input time
@@ -17,11 +16,7 @@ def force(y0,v0,df): ## function returns the force vector [FX,FY] at any input t
     v = v0
     total_force=[]
     for i in range(int(time_of_force/dt)):
-        if r[1] < 0:
-            break
-        else:
-            f_drag = -df*np.linalg.norm(v)*v
-            f_grav = np.array([0,-m*9.8])
+            f_hooks = np.array([0,-ky])
             tot_force = f_drag+f_grav
             a = (f_drag+f_grav)/m
             v += a*dt
@@ -30,47 +25,33 @@ def force(y0,v0,df): ## function returns the force vector [FX,FY] at any input t
     return total_force[-1]
 
 
-def dynamics(y0,v0,df): ##function that solves the system and returns back all the dynamical variables
-    i = 0
-    r = [0,y0]
-    v = v0
+def dynamics(y0,v0,k,time): ##function that solves the system and returns back all the dynamical variables
+    v = [v0]
     t = [0]
-    x = [r[0]]
-    y = [r[1]]
-    vx = [v[0]]
-    vy =[v[1]]
-    while r[1]>=0:
-        i += 1
-        f_drag = -df*np.linalg.norm(v)*v
-        f_grav = np.array([0,-m*9.8])
-        a = (f_drag+f_grav)/m
-        v += a*dt
-        r += v*dt
-        x.append(r[0])
-        y.append(r[1])
-        vx.append(v[0])
-        vy.append(v[1])
-        t.append(i*dt)
-    return t,x,y,vx,vy
+    y = [y0]
+    for j in range(int(time/dt)):
+        f_hooks = -k*y[j]
+        a = (f_hooks)/m
+        v.append(v[j]+a*dt)
+        y.append(y[j]+v[j]*dt)        
+        t.append(j*dt)
+    return t,y,v
 
 
-def energy(t,y,vx,vy): ## function that calculates the total, kinetic, and potential energy
-    y = np.array(y)
-    vx = np.array(vx)
-    vy = np.array(vy)
+def energy(k,y,v): ## function that calculates the total, kinetic, and potential energy
     en = []
     kin=[]
     pot=[]
     for i in range(len(y)):
-        en.append(1/2*m*(vx[i]**2+vy[i]**2)+m*9.8*int(y[i]))
-        kin.append(1/2*m*(vx[i]**2+vy[i]**2))
-        pot.append(m*9.8*y[i])
+        en.append(1/2*m*(v[i]**2)+1/2*k*y[i]**2)
+        kin.append(1/2*m*(v[i]**2))
+        pot.append(1/2*k*y[i]**2)
     return en,kin,pot
 
 
-def plot_dyn(t,x,y,vx,vy): ## function that plots all the dynamical variables in one plot against time
-    var = [x,y,vx,vy]
-    lgnd = ['X vs Time','Y vs time', "VX vs time", 'VY vs time']
+def plot_dyn(t,y,v): ## function that plots all the dynamical variables in one plot against time
+    var = [y,v]
+    lgnd = ['X vs Time', "V vs time",]
     i = 0
     for j in var:
         plt.plot(t,j,label = lgnd[i])
@@ -90,10 +71,15 @@ def plot_energy(t,en,kin,pot): ## function that plots the total energy vs time
 inti = get_int()
 
 ##print(force(inti[0],inti[1],inti[2]))
-var = dynamics(inti[0],inti[1],inti[2])
-en_var = energy(var[0],var[2],var[3],var[4])
+var = dynamics(inti[0],inti[1],inti[2],inti[3])
+en_var = energy(inti[2],var[1],var[2])
+
+##with open("comp/cow_output.txt","w") as f:
+   # f.write("time,x,y\n")
+   # for i,j,k in zip(var[0],var[1],var[2]):
+    #    f.write(f"{i},{j},{k}\n")
 plot_energy(var[0],en_var[0],en_var[1],en_var[2])
-plot_dyn(var[0],var[1],var[2],var[3],var[4])
+plot_dyn(var[0],var[1],var[2])
 
 
 
